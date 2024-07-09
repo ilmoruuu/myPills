ed32537426291dca902aa402beeaac9c4dc1b7f6
RonyRangelSS <rony.rangel@upe.br>
2024-07-08 22:25:35 -0300

    html temporário da página principal

--- projetomypills/mypills/app/views.py
+++ projetomypills/mypills/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the my pills index")
+    return render(request, 'app/index.html')
 
 def login(request):
     return render(request, 'app/login.html')
