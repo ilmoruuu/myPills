e6e2eee687263e0eff5012c54c5f3efd73a0fc07
Murilo Lucena <muriloglucenacanal@gmail.com>
2024-07-16 11:15:10 -0300

    interligacao com o postgresql

--- projetomypills/mypills/mypills/settings.py
+++ projetomypills/mypills/mypills/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mypills.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MyPills',
+        'USER': 'postgres',
+        'PASSWORD': 'ilmoru0407',
+        'HOST': 'loc'
     }
 }
 
